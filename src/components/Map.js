d1f7d1c3435f37bc11d30faa0d85f0147c4b3759
Guk Kim <gukkim132@gmail.com>
2021-03-06 15:15:02 -0600

    Add markers with sample coordinates

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,10 +1,22 @@
 import React, { useState, useCallback, memo } from "react";
-import { GoogleMap, useJsApiLoader } from "@react-google-maps/api";
+import { GoogleMap, Marker, useJsApiLoader } from "@react-google-maps/api";
+
+const sampleCoordinates = [
+  { lat: 43.076599655501866, lng: -89.41248893737793 },
+  { lat: 43.06496001609586, lng: -89.41759435083787 },
+  { lat: 43.07473447236347, lng: -89.38405752182007 },
+  { lat: 43.070216176323775, lng: -89.3565864905864 },
+  { lat: 43.09208318533129, lng: -89.36725616455078 },
+];
 
 const Map = () => {
   const { isLoaded, loadError } = useJsApiLoader({
     id: "google-map-script",
     googleMapsApiKey: "AIzaSyBpzqBpEXNNeLU9O2B5GGqWvspJSk7tfL8",
+  });
+
+  const markers = sampleCoordinates.map((val, ind) => {
+    return <Marker key={ind} position={val} />;
   });
 
   return isLoaded || loadError ? (
@@ -14,7 +26,12 @@ const Map = () => {
         center={center}
         zoom={zoom}
         options={options}
-      ></GoogleMap>
+        onClick={(e) => {
+          console.log(e.latLng.lat(), e.latLng.lng());
+        }}
+      >
+        {markers}
+      </GoogleMap>
     </div>
   ) : (
     "Loading"
