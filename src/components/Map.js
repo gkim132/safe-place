2f0c183b8549cd7fab68c0b960cdf070cce4e7ac
Guk Kim <gukkim132@gmail.com>
2021-03-06 13:44:25 -0600

    Set-up map

---
+++ src/components/Map.js
@@ -0,0 +1,36 @@
+import React, { useState, useCallback, memo } from "react";
+import { GoogleMap, useJsApiLoader } from "@react-google-maps/api";
+import "./Map.css";
+
+const Map = () => {
+  const { isLoaded, loadError } = useJsApiLoader({
+    id: "google-map-script",
+    googleMapsApiKey: "AIzaSyBpzqBpEXNNeLU9O2B5GGqWvspJSk7tfL8",
+  });
+
+  return isLoaded || loadError ? (
+    <div>
+      <GoogleMap
+        mapContainerStyle={mapContainerStyle}
+        center={center}
+        zoom={zoom}
+      ></GoogleMap>
+    </div>
+  ) : (
+    "Loading"
+  );
+};
+
+const mapContainerStyle = {
+  height: "100vh",
+  width: "100vw",
+};
+
+const center = {
+  lat: 43.073051,
+  lng: -89.40123,
+};
+
+const zoom = 12;
+
+export default memo(Map);
