39e5d2fce456c4ab31347e07f41ca19da341a1f2
Guk Kim <gukkim132@gmail.com>
2021-03-16 16:41:17 -0500

    Add spinner during waking Heroku up

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,7 +1,13 @@
 import React, { useState } from "react";
 import "./Register.css";
 
-function Register({ setloadUser, setRoute, setIsSignedIn }) {
+function Register({
+  setloadUser,
+  setRoute,
+  setIsSignedIn,
+  isHerokuAwake,
+  setIsHerokuAwake,
+}) {
   const [registerName, setRegisterName] = useState("");
   const [registerEmail, setRegisterEmail] = useState("");
   const [registerPassword, setRegisterPassword] = useState("");
@@ -19,6 +25,7 @@ function Register({ setloadUser, setRoute, setIsSignedIn }) {
   };
 
   const onSubmitRegister = () => {
+    setIsHerokuAwake(false);
     fetch("https://safe-place-nodejs-backend.herokuapp.com/register", {
       method: "post",
       headers: { "Content-Type": "application/json" },
@@ -32,6 +39,7 @@ function Register({ setloadUser, setRoute, setIsSignedIn }) {
       .then((response) => response.json())
       .then((user) => {
         if (user !== "Register Error" && user.id) {
+          setIsHerokuAwake(true);
           setloadUser(user);
           setRoute("map");
           setIsSignedIn(true);
@@ -47,50 +55,63 @@ function Register({ setloadUser, setRoute, setIsSignedIn }) {
           <fieldset id="sign_up">
             <legend>Register</legend>
             <h5>Safe Place</h5>
-            <div className="control">
-              <input
-                type="text"
-                name="name"
-                id="name"
-                onChange={onNameChange}
-                placeholder="Name"
-              />
-            </div>
-            <div className="control">
-              <input
-                type="email"
-                name="email-address"
-                id="email-address"
-                onChange={onEmailChange}
-                placeholder="Email"
-              />
-            </div>
-            <div className="control">
-              <input
-                type="password"
-                name="password"
-                id="password"
-                onChange={onPasswordChange}
-                placeholder="Password"
-              />
-            </div>
-            {!isRegisterSuccess && (
+            {!isHerokuAwake ? (
               <div>
-                <p>That email is taken. Try another.</p>
+                <img
+                  className="spinner"
+                  src="/spinner-blue.svg"
+                  alt="spinner-solid"
+                />
+                <h5>Signing Up...</h5>
+              </div>
+            ) : (
+              <div>
+                <div className="control">
+                  <input
+                    type="text"
+                    name="name"
+                    id="name"
+                    onChange={onNameChange}
+                    placeholder="Name"
+                  />
+                </div>
+                <div className="control">
+                  <input
+                    type="email"
+                    name="email-address"
+                    id="email-address"
+                    onChange={onEmailChange}
+                    placeholder="Email"
+                  />
+                </div>
+                <div className="control">
+                  <input
+                    type="password"
+                    name="password"
+                    id="password"
+                    onChange={onPasswordChange}
+                    placeholder="Password"
+                  />
+                </div>
+                {!isRegisterSuccess && (
+                  <div>
+                    <p>That email is taken. Try another.</p>
+                  </div>
+                )}
+                <div className="actionButtons">
+                  <div></div>
+                  <div>
+                    <input
+                      type="submit"
+                      value="Register"
+                      onClick={() => {
+                        onSubmitRegister();
+                      }}
+                    />
+                  </div>
+                </div>
               </div>
             )}
-            <div className="actionButtons">
-              <div></div>
-              <div>
-                <input
-                  type="submit"
-                  value="Register"
-                  onClick={() => {
-                    onSubmitRegister();
-                  }}
-                />
-              </div>
-            </div>
           </fieldset>
         </div>
       </main>
